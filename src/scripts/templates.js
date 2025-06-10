cbb6b935825cfb06f8e6dd485fad965c3506ce83
Sincan Maulana <sincanmaulanaa@gmail.com>
2025-06-10 22:41:25 +0700

    Refactor navigation button templates and enhance header styles for improved UI consistency

--- src/scripts/templates.js
+++ src/scripts/templates.js
@@ -30,7 +30,7 @@ export function generateUnauthenticatedNavigationListTemplate() {
 export function generateAuthenticatedNavigationListTemplate() {
   return `
     <li id="push-notification-tools" class="push-notification-tools"></li>
-    <li><a id="new-report-button" class="btn new-report-button" href="#/new">New Story <i class="fas fa-plus"></i></a></li>
+    <li><a id="new-report-button" class="btn new-report-button" href="#/new"><i class="fas fa-plus"></i> New Story</a></li>
     <li><a id="logout-button" class="logout-button" href="#/logout"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
   `;
 }
